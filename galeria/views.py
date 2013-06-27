515c8789da21912e3ef29acd6a852152e84adc9c
Jhonny Cruz <jhonlimaster@gmail.com>
2013-06-27 19:18:06 -0400

    listo para subirlo a heroku

--- galeria/views.py
+++ galeria/views.py
@@ -5,7 +5,8 @@ from galeria.models import Foto
 
 def index(request):
 	f = Foto.objects.all()
-	return render_to_response('home.html', {'fotos': f},context_instance=RequestContext(request))
+	return render_to_response('home.html', {'fotos': f, 'title': 'Mi Galeria'} ,context_instance=RequestContext(request))
 
 def detail(request, foto_id):
-	return HttpResponse("foto: " + foto_id)
+	f = Foto.objects.get(id=foto_id)
+	return render_to_response('detail.html', {'foto': f, 'title': f.fig_caption} ,context_instance=RequestContext(request))
